d08066d11d7d19a055fe4c35af1704cc7ba18045
Victor Hugo Longon <vhlongon@yahoo.com.br>
2016-04-17 22:19:23 +0200

    add notes on the chapeter

--- src/js/containers/book-detail.js
+++ src/js/containers/book-detail.js
@@ -4,13 +4,15 @@ import {connect} from 'react-redux';
 class BookDetail extends Component {
 
   render = () => {
+    console.log(this.props);
     if(!this.props.book) {
       return <div>Select a book to get started.</div>;
     }
     return (
       <div>
         <h3>Details for: </h3>
-        <div>{this.props.book.title}</div>
+        <div>Title: {this.props.book.title}</div>
+        <div>Pages: {this.props.book.pages}</div>
       </div>
     );
   }
